404f6ee5d15812a5f7482d4e57183fa433dc8f15
asebaq <ahmad_sebaq@yahoo.com>
2023-03-02 13:23:13 +0200

    Edit data preparation for lambda

--- explore_rsicd_dataset.py
+++ explore_rsicd_dataset.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from transformers import T5Tokenizer, T5Model
 
-tokenizer = T5Tokenizer.from_pretrained('t5-small', model_max_length=256)
-model = T5Model.from_pretrained('t5-small')
+tokenizer = T5Tokenizer.from_pretrained('t5-base', model_max_length=256)
+model = T5Model.from_pretrained('t5-base')
 
 
 def expand_sentences(sents):
@@ -45,8 +45,8 @@ def prepare_df(base_dir, js_file_name):
     df['sent1'], df['sent2'], df['sent3'], df['sent4'], df['sent5'] = zip(
         *df['sentences'].apply(lambda sents: [sent['raw'] for sent in sents]))
 
-    for i in range(5):
-        df[f'sent{i+1}_emb_t5'] = df[f'sent{i+1}'].apply(embed_sentences)
+    # for i in range(1, 6):
+    #     df[f'sent{i}_emb_t5'] = df[f'sent{i}'].apply(embed_sentences)
 
     for i in range(1, 6):
         df[f'sent{i}_len'] = df[f'sent{i}'].apply(lambda sent: len(sent.split()))
@@ -58,24 +58,25 @@ def prepare_df(base_dir, js_file_name):
 
 
 def main():
-    tokenizer = T5Tokenizer.from_pretrained('t5-small', model_max_length=256)
-    model = T5Model.from_pretrained('t5-small')
-    base_dir = '/home/asebaq/RSICD_optimal'
+    base_dir = '../RSICD_optimal'
     js_file_name = 'dataset_rsicd.json'
     df = prepare_df(base_dir, js_file_name)
-    df = pd.read_csv(os.path.join(base_dir, 'dataset_rsicd.csv'))
-    for i in range(1, 6):
-        print(f'sent{i}_len', df[f'sent{i}_len'].max())
-    sent1 = df.sent1[0]
-    print(sent1)
-    print(len(sent1.split()))
 
-    print(df['class'][df['class'].str.isalpha().values].value_counts())
-    df['class'][df['class'].str.isalpha().values].value_counts().plot(kind='bar')
-    plt.show()
 
-    filename = df.filename[8551]
-    print(filename)
+    # df = pd.read_csv(os.path.join(base_dir, 'dataset_rsicd.csv'))
+    # for i in range(1, 6):
+    #     print(f'sent{i}_len', df[f'sent{i}_len'].max())
+    #
+    # sent1 = df.sent1[0]
+    # print(sent1)
+    # print(len(sent1.split()))
+    #
+    # print(df['class'][df['class'].str.isalpha().values].value_counts())
+    # df['class'][df['class'].str.isalpha().values].value_counts().plot(kind='bar')
+    # plt.show()
+    #
+    # filename = df.filename[8551]
+    # print(filename)
 
 
 if __name__ == '__main__':
